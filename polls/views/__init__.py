ba89cfb18d77e4debe090cbe0d7720ed552f363d
Adam Laf <adamlaf@seznam.cz>
2022-01-30 13:00:06 +0100

    Replace asterisks imports

--- polls/views/__init__.py
+++ polls/views/__init__.py
@@ -1,10 +1,9 @@
 from polls.views.adds import *
-from polls.views.helpers import *
-from polls.views.highscore import *
-from polls.views.history import *
-from polls.views.homepage import *
-from polls.views.login import *
-from polls.views.pie_graph import *
-from polls.views.registration import *
-from polls.views.statistics import *
-from polls.views.userpage import *
+from polls.views.highscore import highscores, load_chart_data
+from polls.views.history import get_history, history
+from polls.views.homepage import god_button, index
+from polls.views.login import login_request
+from polls.views.pie_graph import pie_chart
+from polls.views.registration import register_request
+from polls.views.statistics import load_playerstats, playerstats
+from polls.views.userpage import new_exp_in_library, new_game_in_library, userpage
--- polls/views/adds/__init__.py
+++ polls/views/adds/__init__.py
@@ -1,5 +1,5 @@
-from polls.views.adds.boardgame import *
-from polls.views.adds.expansion import *
-from polls.views.adds.gameplay import *
-from polls.views.adds.player import *
-from polls.views.adds.results import *
+from polls.views.adds.boardgame import add_boardgame, add_boardgame_old, bg_submit
+from polls.views.adds.expansion import add_expansion
+from polls.views.adds.gameplay import add_play, basic_stats, expansions_select_options, load_player_count, randomizer
+from polls.views.adds.player import add_player
+from polls.views.adds.results import add_results
