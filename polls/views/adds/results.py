a3e3b630c1845c6ed404f51f0588ded6da133b51
DejvB <david.bainar@gmail.com>
2022-09-13 00:02:00 +0200

    Shows recent players

--- polls/views/adds/results.py
+++ polls/views/adds/results.py
@@ -5,10 +5,11 @@ from django.shortcuts import render, redirect
 
 from polls.forms import ResultsForm
 
-from ...models import PlayerSpecifics
+from ...models import Player, PlayerSpecifics
 from ..helpers import (
     compute_tournament,
     get_last_gameplay,
+    my_view,
     show_success_tooltip,
     update_elo,
 )
@@ -16,12 +17,16 @@ from ..helpers import (
 
 @login_required
 def add_results(request):
+    userid = my_view(request)
     context = {}
     last_game = get_last_gameplay(request, only_session=False)
     specifics = PlayerSpecifics.objects.filter(
         bg_id_id=last_game.name.id
     ).values_list('id', 'name')
     player_order = [(i, i) for i in range(last_game.NumberOfPlayers + 1)]
+    player_recent_choices = [(q.id, q.name) for q in Player.objects.get(id=userid).get_recent_comrades(id=userid)]
+    player_all_choices = [(q.id, q.name) for q in Player.objects.all().order_by('name')]
+    player_choices = [('-', '------')] + player_recent_choices + [('-', '------')] + player_all_choices
     ResultsFormSet = formset_factory(ResultsForm, extra=0)
     if last_game.with_results:
         formset = ResultsFormSet(
@@ -32,14 +37,6 @@ def add_results(request):
             ],
         )
 
-        for form in formset:
-            form.fields['gp_id'].disabled = True
-            form.fields['player_order'].choices = player_order
-            form.fields['order'].choices = player_order
-            if specifics:
-                form.fields['player_specifics'].choices = specifics
-            else:
-                form.fields.pop('player_specifics')
     else:
         formset = ResultsFormSet(
             request.POST or None,
@@ -49,14 +46,15 @@ def add_results(request):
             ],
         )
 
-        for form in formset:
-            form.fields['gp_id'].disabled = True
-            form.fields['player_order'].choices = player_order
-            form.fields['order'].choices = player_order
-            if specifics:
-                form.fields['player_specifics'].choices = specifics
-            else:
-                form.fields.pop('player_specifics')
+    for form in formset:
+        form.fields['gp_id'].disabled = True
+        form.fields['p_id'].choices = player_choices
+        form.fields['player_order'].choices = player_order
+        form.fields['order'].choices = player_order
+        if specifics:
+            form.fields['player_specifics'].choices = specifics
+        else:
+            form.fields.pop('player_specifics')
     context['formset'] = formset
     if formset.is_valid():
         messages.success(request, 'Form submission successful')
